dd9eb56a8aee37afef491c5a313d6191957ab3ce
Eyyub990 <eyyubga@code.edu.az>
2024-07-20 18:33:55 +0400

    Initial Onion Structure is done!

---
+++ MotiCV/Persistence/PersistenceRegisterModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class PersistenceRegisterModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterAssemblyTypes(this.GetType().Assembly)
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<DbContext>()
+                .As<DbContext>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ MotiCV/WebUI/Program.cs
@@ -0,0 +1,45 @@
+using Domain.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            //builder.Host.UseServiceProviderFactory(new IoCFactory());
+
+            builder.Services.AddRouting(cfg => cfg.LowercaseUrls = true);
+
+            builder.Services.AddDbContext<DbContext>(cfg =>
+            {
+                cfg.UseSqlServer(builder.Configuration.GetConnectionString("cString"));
+            });
+
+            builder.Services.Configure<EmailConfiguration>(cfg => builder.Configuration.GetSection(cfg.GetType().Name).Bind(cfg));
+            builder.Services.Configure<CryptoServiceConfiguration>(cfg => builder.Configuration.GetSection(cfg.GetType().Name).Bind(cfg));
+
+            builder.Services.AddHttpContextAccessor();
+
+
+
+
+            var app = builder.Build();
+            app.UseStaticFiles();
+
+
+
+            app.MapControllerRoute(name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+            app.MapControllerRoute(name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
